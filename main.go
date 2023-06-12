7b9b387c75cf6b6f4d7771e55d17e8eb0db36693
fshalhoub <fshalhoub@hala.com>
2023-06-12 07:31:48 +0300

    FEAT: Init Http Server

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+type response struct {
+	Message string `json:"message"`
+}
+
 func main() {
-	print("Hello World")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(response{Message: "Hello World"})
+	})
+
+	log.Println("listening on", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
